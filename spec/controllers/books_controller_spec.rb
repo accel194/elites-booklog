334771a7fee6307d6137bc5d5b7cca5db6c0632d
accel <accel194@gmail.com>
2017-02-27 22:35:00 +0000

    06_実習

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -3,22 +3,57 @@ require 'rails_helper'
 RSpec.describe BooksController, type: :controller do
   
   describe 'GET /books' do
+    subject { get :index }
+    
+    context 'Bookの情報が無い場合' do
+      it 'Bookの取得数が0であること' do
+        subject
+        books = assigns(:books)
+        expect(books.length).to eq(0)
+      end
+    end
+    
+    context 'Bookの情報がある場合' do
+      before {
+        5.times do
+          create(:book)
+        end
+      }
+      
+      it 'Bookの情報が１つ以上取得できること' do
+        subject
+        books = assigns(:books)
+        expect(books.length).to be >= 1
+      end
+    end
   end
   
   describe 'GET /books/:id' do
   end
-  
-  describe 'GET /books/new' do
+      
+    describe 'GET /books/new' do
     subject { get :new }
     before { login }
       
-    it '@bookに新規Bookオブジェクトが格納されていること' do
-      subject
-      book = assigns(:book)
-      expect(book).to be_a_new(Book)
+      it '@bookに新規Bookオブジェクトが格納されていること' do
+        subject
+        book = assigns(:book)
+        expect(book).to be_a_new(Book)
+      end
+    end
+    
+    
+  describe 'POST /books' do
+    subject {post :create, book: attributes_for(:book) }
+    before { login }
+    
+    it '新しいBookが作成されること' do
+      expect {
+        subject
+      }.to change(Book, :count).by(1)
     end
   end
-    
+  
   describe 'GET /books/:id' do
     let!(:test_book) { create(:book) }
     subject { get :edit, id: test_book.id }
